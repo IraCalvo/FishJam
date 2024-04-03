6994f3ead047b7797da0fa3dc6427b8685f5aaa3
IraCalvo <ira.zephaniah.calvo@gmail.com>
2024-04-02 17:31:42 -0700

    fish movement

--- Assets/Scripts/FishScripts/BasicFish.cs
+++ Assets/Scripts/FishScripts/BasicFish.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class BasicFish : MonoBehaviour
 {
+    [Header("Fish Movement")]
+    [SerializeField] float fishMoveSpeed;
+    [SerializeField] float minLocationPickTimer;
+    [SerializeField] float maxLocationPickTimer;
+    float nextLocationTimer;
+    Vector2 randomTargetPosition;
+    bool isMoving;
+    bool canMove;
+
+    [Header("Money Information")]
     [SerializeField] float minMoneyTimer;
     [SerializeField] float maxMoneyTimer;
     private float moneyTimer;
     [SerializeField] GameObject moneyToDrop;
 
+    [Header("Misc")]
+    Rigidbody2D rb;
+    GameObject tank;
+    Bounds tankBounds;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tank = GameObject.Find("Tank");
+        tankBounds= tank.GetComponent<PolygonCollider2D>().bounds;
+    }
     private void Start()
     {
+        PickRandomLocation();
         ChooseMoneyTimer();
     }
 
@@ -19,17 +42,56 @@ public class BasicFish : MonoBehaviour
         SpawnMoney();
     }
 
+    private void FixedUpdate()
+    {
+        MoveFish();
+    }
+    
     void SpawnMoney()
     {
         if (moneyTimer <= 0)
         {
-            Instantiate(moneyToDrop);
+            Instantiate(moneyToDrop, transform.position, Quaternion.identity);
             ChooseMoneyTimer();
         }
         else
         {
             moneyTimer -= Time.deltaTime;
         }
+    }
+
+    void MoveFish()
+    {
+        if (nextLocationTimer <= 0)
+        {
+            if (Vector2.Distance(transform.position, randomTargetPosition) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, randomTargetPosition, fishMoveSpeed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                PickWaitTimer();
+                PickRandomLocation();
+            }
+        }
+        else
+        { 
+            nextLocationTimer -= Time.deltaTime;
+        }
+    }
+
+    void PickRandomLocation()
+    {
+        float randomX = Random.Range(tankBounds.min.x - 1f, tankBounds.max.x - 1f);
+        float randomY = Random.Range(tankBounds.min.y - 1f, tankBounds.max.y - 1f);
+        
+        randomTargetPosition = new Vector2(randomX, randomY);
+        Debug.Log(randomTargetPosition);
+    }
+
+    void PickWaitTimer()
+    {
+        nextLocationTimer = Random.Range(minLocationPickTimer, maxLocationPickTimer);
     }
 
     void ChooseMoneyTimer()
