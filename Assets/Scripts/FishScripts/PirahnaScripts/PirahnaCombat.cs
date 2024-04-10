5e70f9feac4db0e234f78a72f729ef34c272be7d
IraCalvo <ira.zephaniah.calvo@gmail.com>
2024-04-10 10:26:09 -0700

    Combat changes
    (game was super hard LOL)

--- Assets/Scripts/FishScripts/PirahnaScripts/PirahnaCombat.cs
+++ Assets/Scripts/FishScripts/PirahnaScripts/PirahnaCombat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Internal;
+//using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PirahnaCombat : MonoBehaviour
